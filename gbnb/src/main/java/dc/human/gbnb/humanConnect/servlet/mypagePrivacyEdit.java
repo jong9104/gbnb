20af146f1afa1955d3746f3b5f620d6be08f9555
bandiya47 <bandiya47@naver.com>
2024-06-04 17:36:31 +0900

    Edit*Check added

--- gbnb/src/main/java/dc/human/gbnb/humanConnect/servlet/mypagePrivacyEdit.java
+++ gbnb/src/main/java/dc/human/gbnb/humanConnect/servlet/mypagePrivacyEdit.java
@@ -3,6 +3,7 @@ package dc.human.gbnb.humanConnect.servlet;
 import java.io.IOException;
 
 import dc.human.gbnb.humanConnect.dao.mypagePrivacyEditDAO;
+import dc.human.gbnb.humanConnect.dto.UserDTO;
 import dc.human.gbnb.humanConnect.dto.mypagePrivacyEditDTO;
 import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletConfig;
@@ -11,18 +12,19 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class mypage_privacy_edit
  */
 @WebServlet("/privacyEdit")
-public class mypage_privacy_edit extends HttpServlet {
+public class mypagePrivacyEdit extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * Default constructor. 
      */
-    public mypage_privacy_edit() {
+    public mypagePrivacyEdit() {
         // TODO Auto-generated constructor stub
     }
 
@@ -39,14 +41,11 @@ public class mypage_privacy_edit extends HttpServlet {
 	public void destroy() {
 		// TODO Auto-generated method stub
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		UserDTO user = (UserDTO) session.getAttribute("user");
 		request.setCharacterEncoding("utf-8");
-		String newName=request.getParameter("u_name");
-		String newId=request.getParameter("u_id");
+		String newName=request.getParameter("u_name");		
 		String newPwd=request.getParameter("u_pwd");
 		String newPwdCheck=request.getParameter("u_pwdCheck");
 		String newAddr=request.getParameter("u_addr");
@@ -55,7 +54,7 @@ public class mypage_privacy_edit extends HttpServlet {
 		
 		mypagePrivacyEditDTO editDTO=new mypagePrivacyEditDTO();
 		
-		
+		editDTO.setId(user.getUserId());
 		editDTO.setPw(newPwd);
 		editDTO.setEmail(newEmail);
 		editDTO.setPhone(newPhone);
@@ -65,8 +64,7 @@ public class mypage_privacy_edit extends HttpServlet {
 		mypagePrivacyEditDAO editDAO=new mypagePrivacyEditDAO();
 		editDAO.privacyEdit(editDTO);
 		
-		
-		
+		response.sendRedirect("mypagePrivacyEdit.jsp");
 	}
 
 }
